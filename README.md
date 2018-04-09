33097d6f8697bb88cf1df4090864ec9504597a0a
Alan Synn <alan@alansynn.com>
2018-04-09 16:43:53 +0900

    Rearrange Project files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-study.document
+# Python Algorithms
+
+## [Programmers]((https://programmers.co.kr))
++ [문자열 다루기 기본](https://programmers.co.kr/learn/challenge_codes/99)
+    + [풀이](programmers/01.py)
+
++ [가운데 글자 가져오기](https://programmers.co.kr/learn/challenge_codes/82)
+    + [풀이](programmers/02.py)
+
+## [Leetcode](https://leetcode.com)
--- programmers/02.py
+++ programmers/02.py
@@ -4,9 +4,10 @@ def string_middle(str):
     if buf % 2 == 1:
         return str[value]
     elif buf % 2 == 0:
-        return str[value - 1 : value + 1]
+        return str[value - 1: value + 1]
+
 
 print(string_middle("power"))
 print(string_middle("Here"))
 
-##https://programmers.co.kr/learn/challenge_codes/82
+# https://programmers.co.kr/learn/challenge_codes/82
