e85369766be78ea1ed0ec2614d23045401ee3808
heewon <luke@akaon.com>
2018-04-13 18:00:21 +0900

    revise 05

--- programmers/05.py
+++ programmers/05.py
@@ -1,14 +1,5 @@
 def sum_digit(number):
-    result = 0
-    for i in range(0, 1000):
-        if number < pow(10, i):
-            break;
+    return sum([int(i) i in str(number)])
 
-    for j in range(i, 0, -1):
-        result += number // pow(10, j - 1)
-        number -= number // pow(10, j - 1) * pow(10, j - 1)
-
-    return result
-
-print(" result  : {}".format(sum_digit(123)));
+print(" result  : {}".format(sum_digit(123)))
 # https://programmers.co.kr/learn/challenge_codes/116/solutions
