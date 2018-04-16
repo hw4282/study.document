d33851bb9a675e2bd855337d5716f6eed57e23a2
heewon <luke@akaon.com>
2018-04-16 14:14:28 +0900

    revise1 06

--- programmers/06.py
+++ programmers/06.py
@@ -1,7 +1,4 @@
 def number_generator(x,n):
-    a = []
-    for i in range(x, x*(n+1), +x):
-        a.append(i)
-    return a
+    return [x * i + x for i in range(1,n+1)]
 
 print(number_generator(3,5))
