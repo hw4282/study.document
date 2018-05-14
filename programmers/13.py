da59a06e30d165f2c6a4102ef0d865f2af48178d
heewon <luke@akaon.com>
2018-05-14 17:57:23 +0900

    revise 13 Grammer PEP8

--- programmers/13.py
+++ programmers/13.py
@@ -10,23 +10,17 @@ def nextBigNumber(n):
     for buf in range(len(b) - 1, 1, -1):
         if b[buf - 1] == '0' and b[buf] == '1':
             break
-
         elif b[buf - 1] == '1' and b[buf] == '1':
             count += 1
-
         if b[buf] == '0':
             sig += 1
-
-
     b[buf - 1], b[buf] = 1, 0
 
     if count and sig >= 1:
         for a in range(1, sig + 1):
             b[buf + a], b[buf + count + a] = 0, 1
-
-    for i in b :
+    for i in b:
         answer = str(answer) + str(i)
-
     return int(answer, 2)
 
 print(nextBigNumber(78))
